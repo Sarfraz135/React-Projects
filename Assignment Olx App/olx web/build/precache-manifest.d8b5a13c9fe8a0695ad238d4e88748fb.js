33fc4b8a4115d2c5619652aac66d10502fda01a8
Sarfraz Aslam <82870539+Sarfraz135@users.noreply.github.com>
2021-04-21 14:31:42 -0700

    Add files via upload

---
+++ Assignment Olx App/olx web/build/precache-manifest.d8b5a13c9fe8a0695ad238d4e88748fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851c1a640009bcbc42b1a3202fda1545",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1379b881536358a7e8f4",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a922cc97224ccc3d7c8e",
+    "url": "/static/css/main.ddd21dde.chunk.css"
+  },
+  {
+    "revision": "1379b881536358a7e8f4",
+    "url": "/static/js/2.4d3e0386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d3e0386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a922cc97224ccc3d7c8e",
+    "url": "/static/js/main.f17b7176.chunk.js"
+  },
+  {
+    "revision": "986ff8f6a72b72346bb8",
+    "url": "/static/js/runtime-main.e5203a96.js"
+  },
+  {
+    "revision": "428d07572749b541b02cfb359a466874",
+    "url": "/static/media/olx1.428d0757.PNG"
+  }
+]);
